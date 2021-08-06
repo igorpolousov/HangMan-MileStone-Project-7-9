5e2ccab6b2b852c81929ed65763a1dd6a7fba8d1
Igor Polousov <igorpolousov@gmail.com>
2021-08-06 13:03:53 +0300

    Initial Commit

---
+++ HangMan-MileStone Project 7-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HangMan-MileStone Project 7-9
+//
+//  Created by Igor Polousov on 06.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
